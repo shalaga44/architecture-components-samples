1d4631e5e9af72b758494e2203fcee593f787746
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-07-08 14:01:57 +0200

    viewBindindSample Pass Test

--- viewModelSample/app/src/main/java/com/shalaga44/viewmodelsample/BindFragment.kt
+++ viewModelSample/app/src/main/java/com/shalaga44/viewmodelsample/BindFragment.kt
@@ -1,59 +1,25 @@
 package com.shalaga44.viewmodelsample
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.shalaga44.viewmodelsample.databinding.FragmentBindBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BindFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class BindFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class BindFragment : Fragment(R.layout.fragment_bind) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private var fragmentBindFragment: FragmentBindBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentBindBinding.bind(view)
+        fragmentBindFragment = binding
+        binding.textViewFragment.text = getString(R.string.hello_from_vb_bind_fragment)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bind, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BindFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BindFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDetach() {
+        fragmentBindFragment = null
+        super.onDetach()
     }
 }
