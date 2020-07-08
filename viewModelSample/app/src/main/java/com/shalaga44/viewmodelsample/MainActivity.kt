fbb7ae7a0fea7971e291735ae547c11fcbdc7cef
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-07-08 14:01:57 +0200

    viewModelSample Pass Test

--- viewModelSample/app/src/main/java/com/shalaga44/viewmodelsample/MainActivity.kt
+++ viewModelSample/app/src/main/java/com/shalaga44/viewmodelsample/MainActivity.kt
@@ -2,10 +2,20 @@ package com.shalaga44.viewmodelsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shalaga44.viewmodelsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+    override fun onStart() {
+        binding.activityTextView.text = getString(R.string.hello_from_vb_activity)
+        super.onStart()
     }
 }
