9278bec46fe29fb731bc43602ab7002f3dd24f4c
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-07-09 22:44:22 +0200

    Result from time transformation

--- LiveDataSample/app/src/main/java/com/shalaga44/livedatasample/MainActivityViewModel.kt
+++ LiveDataSample/app/src/main/java/com/shalaga44/livedatasample/MainActivityViewModel.kt
@@ -2,9 +2,23 @@ package com.shalaga44.livedatasample
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
+import androidx.lifecycle.switchMap
+import kotlinx.coroutines.delay
+import java.util.*
 
-class MainActivityViewModel(private val dataSource: DataSource) : ViewModel()  {
+class MainActivityViewModel(private val dataSource: DataSource) : ViewModel() {
     val currentTime = dataSource.getCurrentTime()
+
+    val currentTimeTransformed = currentTime.switchMap {
+        liveData { emit(timeStampToTime(it)) }
+    }
+
+    suspend fun timeStampToTime(long: Long): String {
+        delay(100)
+        val date = Date(long)
+        return date.toString()
+    }
 
 }
 
