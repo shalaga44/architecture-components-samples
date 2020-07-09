681b9a4eea4bf55726712357f250707f8d0b6c56
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-07-09 21:48:50 +0200

    CREATE MainActivityViewModel & it Factory

--- LiveDataSample/app/src/main/java/com/shalaga44/livedatasample/MainActivityViewModel.kt
+++ LiveDataSample/app/src/main/java/com/shalaga44/livedatasample/MainActivityViewModel.kt
@@ -1,4 +1,17 @@
 package com.shalaga44.livedatasample
 
-class MainActivityViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivityViewModel : viewModel() {
+
+}
+
+object MainActivityVMFActory : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainActivityViewModel::class.java)) {
+            return MainActivityViewModel() as T
+        }
+        throw IllegalArgumentException("Unknown view model class")
+    }
 }
