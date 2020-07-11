56fa0efaacc05f2462b1ecff170769511b9eed15
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-07-11 14:51:49 +0200

    Add viewTransactionsFragment

---
+++ NavigationBasicSample/app/src/main/java/com/shalaga44/navigationsample/viewTransactionsFragment.kt
@@ -0,0 +1,12 @@
+package com.shalaga44.navigationsample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class viewTransactionsFragment : Fragment() {
+
+}
