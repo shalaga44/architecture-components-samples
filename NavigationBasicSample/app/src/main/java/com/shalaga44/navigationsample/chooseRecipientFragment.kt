627a141a5ba50c9a3e38d6a50b50308e65803461
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-07-11 15:20:00 +0200

    Add chooseRecipientFragment

---
+++ NavigationBasicSample/app/src/main/java/com/shalaga44/navigationsample/chooseRecipientFragment.kt
@@ -0,0 +1,17 @@
+package com.shalaga44.navigationsample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.shalaga44.navigationsample.databinding.FragmentChooseRecipientBinding
+
+
+class chooseRecipientFragment : Fragment() {
+        private lateinit var binding: FragmentChooseRecipientBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentChooseRecipientBinding.bind(view)
+    }
+}
