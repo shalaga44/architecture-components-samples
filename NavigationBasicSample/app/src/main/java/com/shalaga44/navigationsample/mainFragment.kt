fc41323c31f70bd3ea67328bb2d9d139a7e545f7
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-07-11 17:12:34 +0200

    Add Navigation Graph

--- NavigationBasicSample/app/src/main/java/com/shalaga44/navigationsample/mainFragment.kt
+++ NavigationBasicSample/app/src/main/java/com/shalaga44/navigationsample/mainFragment.kt
@@ -1,15 +1,13 @@
 package com.shalaga44.navigationsample
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.shalaga44.navigationsample.databinding.FragmentMainBinding
 
 
 class mainFragment : Fragment() {
-   private lateinit var binding: FragmentMainBinding
+    private lateinit var binding: FragmentMainBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMainBinding.bind(view)
