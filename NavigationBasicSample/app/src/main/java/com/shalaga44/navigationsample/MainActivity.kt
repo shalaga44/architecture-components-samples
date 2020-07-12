450308009a8daf078a365102b2c3edbec99d3561
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-07-12 05:13:05 +0200

    Add navigation support to the default action bar

--- NavigationBasicSample/app/src/main/java/com/shalaga44/navigationsample/MainActivity.kt
+++ NavigationBasicSample/app/src/main/java/com/shalaga44/navigationsample/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.shalaga44.navigationsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
