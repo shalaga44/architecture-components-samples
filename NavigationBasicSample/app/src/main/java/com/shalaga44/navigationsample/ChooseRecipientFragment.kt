a223dafe05eef351c5f136baf08d50bfc2f86bc0
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-07-12 04:15:56 +0200

    Add Deep Link

--- NavigationBasicSample/app/src/main/java/com/shalaga44/navigationsample/ChooseRecipientFragment.kt
+++ NavigationBasicSample/app/src/main/java/com/shalaga44/navigationsample/ChooseRecipientFragment.kt
@@ -1,27 +1,28 @@
 package com.shalaga44.navigationsample
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
 import com.shalaga44.navigationsample.databinding.FragmentChooseRecipientBinding
 
 
 class ChooseRecipientFragment : Fragment(R.layout.fragment_choose_recipient) {
-        private lateinit var binding: FragmentChooseRecipientBinding
+    private val args: ChooseRecipientFragmentArgs by navArgs()
+    private lateinit var binding: FragmentChooseRecipientBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentChooseRecipientBinding.bind(view)
 
+        binding.textInputLayout.editText?.setText(args.username)
         binding.cancelButton.setOnClickListener {
             activity!!.onBackPressed()
         }
         binding.sendButton.setOnClickListener {
             val username = binding.textInputLayout.editText?.text.toString()
             val action = ChooseRecipientFragmentDirections
-            .actionChooseRecipientFragmentToSpacifyAmountFragment(username)
+                .actionChooseRecipientFragmentToSpacifyAmountFragment(username)
             Navigation.findNavController(view)
                 .navigate(action)
         }
