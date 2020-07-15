6e62df2f03e00d271a50ffc0cd0ffd7685319854
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-07-15 22:04:37 +0200

    CREATE Login Activity & and fragments

---
+++ hiltSample/app/src/main/java/com/shalaga44/hiltsample/ui/login/LoginFragment.kt
@@ -0,0 +1,9 @@
+package com.shalaga44.hiltsample.ui.login
+
+import androidx.fragment.app.Fragment
+import com.shalaga44.hiltsample.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+
+}
